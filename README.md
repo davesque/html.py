51262a83e0ea380f9fd695227b62139e4615419f
David Sanders <davesque@gmail.com>
2015-10-18 14:39:29 -0600

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+# html.py
+
+A toy html parser written in pure python.
+
+## License
+
+Please refer to `LICENSE.md` in this repo.
--- setup.py
+++ setup.py
@@ -8,4 +8,10 @@ setup(
     version=parse_html.version,
     packages=find_packages(),
     test_suite='parse_html.tests',
+    install_requires=[
+        'parsing==0.1',
+    ],
+    dependency_links=[
+        'https://github.com/davesque/parsing.py/tarball/master#egg=parsing-0.1',
+    ],
 )
